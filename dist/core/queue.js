7f85813f90b5e87d8f44c636c6ed66c63af8d370
Timmy Herzog <118983496+Snaxerl@users.noreply.github.com>
2026-09-18 12:26:31 +0200

    ..

--- dist/core/queue.js
+++
@@ -1,9 +0,0 @@
-/** A failed operation must not poison subsequent writes. */
-export class SerialQueue {
-    tail = Promise.resolve();
-    enqueue(operation) {
-        const result = this.tail.then(operation, operation);
-        this.tail = result.catch(() => undefined);
-        return result;
-    }
-}
